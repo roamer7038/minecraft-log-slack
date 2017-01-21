90c5426dbee4f5bb98ae8951444805b4e1e4b76a
roamer7038 <okayado7038@gmail.com>
2017-01-21 10:58:19 +0900

    コンフィグファイルの作成

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Minecraft-Log-Slack
-Minecraftのログを監視してSlackに通知する
+ Minecraftのログを監視してSlackに通知する．
+ 
+ ![スクリーンショット](https://raw.githubusercontent.com/roamer7038/minecraft-log-slack/image/screenshot.png)
+
+## Setting
+ `config.json`を編集して．
+ 
+ * path: `latest.log`のパス
+ * apiKey: SlackのAPIキー
+ * lang: `en` か `ja`
+ * channel: Slackチャンネル名
+ 
+## Usage
+ pm2 のインストール
+
+`sudo npm install pm2 -g`
+
+ 自動起動の登録
+
+`sudo pm2 startup centos`
+
+ pm2 にスクリプトを登録
+
+`sudo pm2 start app.js`
+
+## License
+MIT
+
+Copyright (c) 2015-2017 @roamer7038 
--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 'use strict';
+
 const Tail = require('tail').Tail;
 const Slack = require('./lib/post-slack');
+const config = require('./config.json');
 
-// ログファイル
-const filename = "../logs/latest.log";
-const tail = new Tail(filename);
-
-// Slack API キー
-const slackApiKey = '';
-const slack = new Slack('log', slackApiKey, 'ja');
+const tail = new Tail(config.path);
+const slack = new Slack(config.channel, config.apiKey, config.lang);
 
 // ログ監視
-tail.on('line', (data)=> {
-    let post = slack.postMessage(data);
-    if(post) console.log(post);
+tail.on('line', (data) => {
+  let post = slack.postMessage(data);
+  if (post) console.log(post);
 });
 
-tail.on('error', (data)=> {
-    console.log("error:", data);
+tail.on('error', (data) => {
+  console.log("error:", data);
 });
